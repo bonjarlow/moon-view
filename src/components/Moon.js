f62085342ed86377e458b5d007352932ef6d173c
bonjarlow <jon.michael.barlow@gmail.com>
2025-08-03 19:19:57 -0400

    moon texture

--- src/components/Moon.js
+++ src/components/Moon.js
@@ -1,10 +1,12 @@
 import * as THREE from "three";
 import { useRef, useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import { latLonToVector3, getSublunarLatLon } from "../utils/astroUtil";
 
 export default function Moon({ moonPos, orbScale }) {
   const moonRef = useRef();
+  const moonTexture = useLoader(THREE.TextureLoader, "/textures/lroc_color_poles_1k.jpg");
   const moonColor = "gray";
 
   return (
@@ -12,7 +14,7 @@ export default function Moon({ moonPos, orbScale }) {
       {/* Moon mesh */}
       <mesh castShadow receiveShadow position={moonPos}>
         <sphereGeometry args={[0.3*orbScale, 128, 128]} />
-        <meshStandardMaterial color={moonColor} />
+        <meshStandardMaterial map={moonTexture} />
       </mesh>
     </>
   );
